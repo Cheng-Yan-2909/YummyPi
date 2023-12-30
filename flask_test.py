d5a5c8754f4481e1b70fc255a4d17d939cf61e46
Cheng Yan <cheng.yan.2909@gmail.com>
2023-12-30 12:35:36 -0600

    save -- sample requests

--- flask_test.py
+++ flask_test.py
@@ -1,44 +1,41 @@
-from falsk import Flask, request
+from flask import Flask, request
 import os
 import time
 import json
+import RPi.GPIO as GPIO
 
 
 app = Flask(__name__)
 
-@app.route("/", defaults={'path':''})
-@app.route(/<path:path>, methods=['POST', 'GET'])
+STATUSON = ["on", "switch on", "enable", "power on", "activate", "turn on", "kai"] 
+STATUSOFF = ["off", "switch off", "deactivate", "power off", "disable", "turn off", "guan"]
+
+
+@app.route("/", methods=['POST', 'GET'])
 def default_route():
-   request_data = ""
-   request_args = ""
-   request_form = ""
+    request_data = request.get_json()
+    print( json.dumps(request_data), flush=True )
+    print("============================================", flush=True)
+    fan_pin = 4
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(fan_pin,GPIO.OUT)
 
-   try:
-       request_data = json.dumps(request.get_json())
-   except:
-       pass
+    if "intent" in request_data["request"]:
+        intent = request_data["request"]["intent"]
+        if intent["name"] == "FanIntent":
+            val = intent["slots"]["status"]["value"]
+            if val in STATUSON:
+                GPIO.output(fan_pin,GPIO.HIGH)
+                print("Fan is on")
+            elif val in STATUSOFF:
+                GPIO.output(fan_pin,GPIO.LOW)
+                print("Fan is off")
+            else:
+                print("unknown state")
+   
 
-   try:
-       request_args = json.dumps(request.args)
-   except:
-       pass
-
-   try:
-       request_form = json.dumps(request.form)
-   except:
-       pass
-
-
-
-   data = {
-      "request_data": request_data,
-      "request_args": request_args,
-      "reqeust_form": request_form
-   }
-
-   print( json.dumps(data), flush=True )
-
-   return "hello", 200
+    return "hello", 200
 
 
 
