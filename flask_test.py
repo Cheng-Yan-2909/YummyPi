a7c8be729ba86a99cd8e180f5c01cdb0d9b0b9ef
Cheng Yan <chng.yan.2909@gmail.com>
2023-12-31 17:17:19 -0600

    debug

--- flask_test.py
+++ flask_test.py
@@ -6,11 +6,15 @@ from RPi import GPIO
 from xml.etree import ElementTree
 import inspect
 
+true = True
+false = False
+
+
 app = Flask(__name__)
 
 STATUS_ON = ["on", "switch on", "enable", "power on", "activate", "turn on", "kai"]
 STATUS_OFF = ["off", "switch off", "deactivate", "power off", "disable", "turn off", "guan"]
-
+user_id_file = "/tmp/user_id"
 
 def get_speach(text):
     try:
@@ -49,42 +53,98 @@ def speach(text, card_title=None, card_content=None, endSession=True):
     return json.dumps(response_wrapper)
 
 
-def process_alexa_request(request_data):
-    print("============== Request Data ================", flush=True)
-    print(json.dumps(request_data, indent=4), flush=True)
-    print("============================================", flush=True)
+def fan_intent(val):
     fan_pin = 4
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(fan_pin, GPIO.OUT)
 
+    if val in STATUS_ON:
+        GPIO.output(fan_pin, GPIO.HIGH)
+        text = "Fan is now on"
+        s = "on"
+    elif val in STATUS_OFF:
+        GPIO.output(fan_pin, GPIO.LOW)
+        text = "Fan is now off"
+        s = "off"
+    else:
+        text = f"Unknown FanIntent value {val}"
+
+    return speach(text, "Fan Status", f"Fan is currently {s}")
+
+
+def device_intent(val):
+    pass
+
+
+def get_user_id(request_data):
+    user_id = None
+    if "session" in request_data:
+        try:
+            user_id = request_data["session"]["user"]["userId"]
+        except:
+            user_id = None
+
+    if user_id is None and "context" in request_data:
+        try:
+            user_id = request_data["context"]["System"]["user"]["userId"]
+        except:
+            user_id = None
+
+    return user_id
+
+def save_user_id(request_data):
+    user_id = get_user_id(request_data)
+    if user_id is not None:
+        with open(user_id_file, "w") as w:
+            w.write(user_id)
+            w.close()
+
+
+def check_user_id(request_data):
+    user_id = None
+    if os.path.exists(user_id_file):
+        with open(user_id_file) as r:
+            user_id = r.read()
+            r.close()
+
+        if user_id == get_user_id(request_data):
+            return True
+
+        return false
+
+def process_alexa_request(request_data):
+    print("============== Request Data ================", flush=True)
+    print(json.dumps(request_data, indent=4), flush=True)
+    print("============================================", flush=True)
+
+
     text = "Unknown Operation"
     s = "unknown"
 
     if "request" in request_data:
+        if not check_user_id(request_data):
+            return "Bad Request", 400
+
         if "intent" in request_data["request"]:
             intent = request_data["request"]["intent"]
-            if intent["name"] == "FanIntent":
-                val = intent["slots"]["status"]["value"]
-                if val in STATUS_ON:
-                    GPIO.output(fan_pin, GPIO.HIGH)
-                    text = "Fan is now on"
-                    s = "on"
-                elif val in STATUS_OFF:
-                    GPIO.output(fan_pin, GPIO.LOW)
-                    text = "Fan is now off"
-                    s = "off"
-                else:
-                    text = f"Unknown FanIntent value {val}"
+            val = intent["slots"]["status"]["value"]
+            if val == "FanIntent":
+                return fan_intent(val)
 
-                return speach(text, "Fan Status", f"Fan is currently {s}")
+            elif val == "DeviceIntent":
+                return device_intent(val)
+
             else:
                 text = f"Not sure what to do with intent {intent['name']}"
                 return speach(text, "Fan Status", f"Fan is currently {s}")
 
         elif "type" in request_data["request"]:
             t = request_data["request"]["type"]
-            print(f"================= unknown request type: {t} ====================", flush=True)
+            if "LaunchRequest" == request_data["request"]["type"]:
+                save_user_id(request_data)
+
+            print(f"================= request type: {t} ====================", flush=True)
             return speach(f"Daniel Toy is {t}", "Fan Status", "Fan initialized", False)
 
         else:
@@ -113,8 +173,11 @@ def default_route():
 
 
 if __name__ == '__main__':
-   port = 8000
-   app.run(host='127.0.0.1', port=port)
+    if os.path.exists(user_id_file):
+        print(f"Removing existing user id file: {user_id_file}", flush=True)
+        os.remove(user_id_file)
+    port = 8000
+    app.run(host='127.0.0.1', port=port)
 
 
 
