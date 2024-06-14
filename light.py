43491adf4624e25ee0ef4c80d92509090aadc548
Cheng Yan <cheng.yan.2909@gmail.com>
2024-06-14 15:57:27 -0500

    flashing light

--- light.py
+++ light.py
@@ -1,5 +1,6 @@
 from RPi import GPIO
 import sys
+import time
 
 state = True
 light_color_func = "all"
@@ -13,36 +14,54 @@ RED_LED = 17
 BLUE_LED = 27
 GREEN_LED = 22
 WHITE_LED = 23
+
+#
+# initialize the device
+#
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
 
-def all():
-    red()
-    blue()
-    green()
-    white()
+def all(state):
+    for i in range(0, 20):
+        red(state)
+        time.sleep(0.1)
+        blue( state)
+        time.sleep(0.1)
+        green(state)
+        time.sleep(0.1)
+        white(state)
+        time.sleep(0.1)
+        state = not state
+        white(state)
+        time.sleep(0.1)
+        green(state)
+        time.sleep(0.1)
+        blue(state)
+        time.sleep(0.1)
+        red(state)
+        time.sleep(0.1)
+        state = not state
 
-
-def red():
+def red(status):
     GPIO.setup(RED_LED, GPIO.OUT)
-    GPIO.output(RED_LED, state)
+    GPIO.output(RED_LED, status)
 
 
-def blue():
+def blue(status):
     GPIO.setup(BLUE_LED, GPIO.OUT)
-    GPIO.output(BLUE_LED, state)
+    GPIO.output(BLUE_LED, status)
 
 
-def green():
+def green(status):
     GPIO.setup(GREEN_LED, GPIO.OUT)
-    GPIO.output(GREEN_LED, state)
+    GPIO.output(GREEN_LED, status)
 
 
-def white():
+def white(status):
     GPIO.setup(WHITE_LED, GPIO.OUT)
-    GPIO.output(WHITE_LED, state)
+    GPIO.output(WHITE_LED, status)
 
 
-globals()[light_color_func]()
+globals()[light_color_func](state)
 
