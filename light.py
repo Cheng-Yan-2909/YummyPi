4be31256e4b22585a7d05e2ae8353e3714a54cd1
Cheng Yan <cheng.yan.2909@gmail.com>
2024-06-22 22:08:52 -0500

    7-segment

--- light.py
+++ light.py
@@ -14,6 +14,7 @@ RED_LED = 17
 BLUE_LED = 27
 GREEN_LED = 22
 WHITE_LED = 23
+YELLOW_LED = 24
 
 #
 # initialize the device
@@ -22,26 +23,86 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
 
+
 def all(state):
-    for i in range(0, 20):
-        red(state)
-        time.sleep(0.1)
-        blue( state)
-        time.sleep(0.1)
-        green(state)
-        time.sleep(0.1)
-        white(state)
-        time.sleep(0.1)
-        state = not state
-        white(state)
-        time.sleep(0.1)
-        green(state)
-        time.sleep(0.1)
-        blue(state)
-        time.sleep(0.1)
-        red(state)
-        time.sleep(0.1)
-        state = not state
+    for i in range(0, 2):
+        blueon(state, 1)
+        blueon(state, 1)
+        blueon(state, 3)
+        blueon(state, 1)
+        blueon(state, 1)
+        blueon(state, 3)
+        blueon(state, 1)
+        redon(state, 1)
+        whiteon(state, 1)
+        greenon(state, 1)
+        blueon(state, 1)
+        time.sleep(3)
+        redon(state, 1)
+        redon(state, 1)
+        redon(state, 1)
+        redon(state, 1)
+        redon(state, 1)
+        blueon(state, 1)
+        blueon(state, 1)
+        blueon(state, 1)
+        blueon(state, 1)
+        greenon(state, 1)
+        greenon(state, 1)
+        blueon(state, 1)
+        greenon(state, 1)
+        redon(state, 1)
+
+
+
+
+
+
+
+
+# red(state)
+#         red(state)
+#         time.sleep(0.1)
+#         blue( state)
+#         time.sleep(0.1)
+#         green(state)
+#         time.sleep(0.1)
+#         white(state)
+#         time.sleep(0.1)
+#         state = not state
+#         white(state)
+#         time.sleep(0.1)
+#         green(state)
+#         time.sleep(0.1)
+#         blue(state)
+#         time.sleep(0.1)
+#         red(state)
+#         time.sleep(0.1)
+#         state = not state
+
+
+def redon(state, _time):
+    red(state)
+    time.sleep(_time)
+    red(not state)
+
+
+def blueon(state, _time):
+    blue(state)
+    time.sleep(_time)
+    blue(not state)
+
+
+def greenon(state, _time):
+    green(state)
+    time.sleep(_time)
+    green(not state)
+
+
+def whiteon(state, _time):
+    white(state)
+    time.sleep(_time)
+    white(not state)
 
 def red(status):
     GPIO.setup(RED_LED, GPIO.OUT)
@@ -61,6 +122,11 @@ def green(status):
 def white(status):
     GPIO.setup(WHITE_LED, GPIO.OUT)
     GPIO.output(WHITE_LED, status)
+
+
+def yellow(status):
+    GPIO.setup(YELLOW_LED, GPIO.OUT)
+    GPIO.output(YELLOW_LED, status)
 
 
 globals()[light_color_func](state)
--- test.py
+++ test.py
@@ -2,30 +2,37 @@ from RPi import GPIO
 import sys
 import time
 
-all_out = [ 23, 24, 17, 27 ]
+all_out = [ 17, 27, 22, 23, 24, 25, 6, 13 ]
 state = True
-port = 23
+port = None
 
 if len(sys.argv) > 1:
     arg = sys.argv[1].lower()
+    if "=" in arg:
+        port, arg = arg.split("=")
     state = arg == "true" or arg == "yes" or arg == "on"
-
-
-if len(sys.argv) > 2:
-    arg = int(sys.argv[2])
-    print(f"port arg is '{arg}'")
-    if arg in all_out:
-        port = arg
-    else:
-        print(f"... '{arg}' not in {all_out}")
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-for w in all_out:
-    GPIO.setup(w, GPIO.OUT)
-    GPIO.output(w, False)
 
-print(f"port: {port} is set to {state}")
-GPIO.output(port, state)
+#
+# negative logic
+#
+def setState(_port, _state):
+    _port = int(_port)
+    GPIO.setup(_port, GPIO.OUT)
+    GPIO.output(_port, not _state)
+
+
+if port is None:
+    for w in all_out:
+        setState(w, state)
+else:
+    port = int(port)
+    if port not in all_out:
+        print(f"invalid port: {port}")
+    else:
+        print(f"port: {port} is set to {state}")
+        setState(port, state)
 
